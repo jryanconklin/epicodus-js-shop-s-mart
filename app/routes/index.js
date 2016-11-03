067d7920e079324853ce2426f391d7289a3dca45
J. Ryan Conklin and Stephen Emery <j.+stephen@>
2016-11-03 16:29:13 -0700

    Add featured game to index.hbs.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  model: function() {
+    var random = Math.floor(Math.random() * 12) + 1;
+    return this.store.query('item', {
+      orderBy: 'inventory',
+      startAt: random,
+      limitToFirst: 1
+    });
+  }
 
 
 });
