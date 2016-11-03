edf1a98c0d7582a5b904be01a64619af3154d4de
J. Ryan Conklin and Stephen Emery <j.+stephen@>
2016-11-03 09:54:31 -0700

    Add Initial Display Cart Component

--- app/routes/store/item.js
+++ app/routes/store/item.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('item', params.item_id);
-  }
-});
+  },
+
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    }
+  } // End Actions
+}); // End Ember.Route
